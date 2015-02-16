850486afc179f8f14085b97640d994ca48e82fa3
Jonathon Gray <jonathon@thesnippetapp.com>
2015-02-15 23:18:46 -0800

    fixed folders

---
+++ admin/firstdata.php
@@ -0,0 +1,7 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import joomla controller library
+jimport('joomla.application.component.controller');
+ 
